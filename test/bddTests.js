2e57d4dac9d71e397c80f4e1a474c2c85cc9b093
andreiGladkov <gladkov@intervolga.ru>
2015-07-27 20:04:11 +0300

    add test for gray filter

--- test/bddTests.js
+++ test/bddTests.js
@@ -5,21 +5,48 @@
 var canvas = document.createElement('canvas'),
 	ctx = canvas.getContext('2d'),
 	entityCanvas = new InCanvas(canvas),
+	grayCanvas,
 	nodeSectionBaseCanvas = document.getElementById('base-canvas'),
 	nodeSectionEditCanvas = document.getElementById('edit-canvas'),
+	pixelCount = canvas.width*canvas.height,
 	img = new Image();
 
 img.src = '/inCanvas/test/img/img.jpg';
 
 img.onload = function(){
-
 	canvas.width = this.naturalWidth;
 	canvas.height = this.naturalHeight;
 	ctx.drawImage(img, 0, 0);
 	entityCanvas = new InCanvas(canvas); // Перезапишем сущность после загрузки изображения
+	grayCanvas = entityCanvas.filter('white-black');
 	nodeSectionBaseCanvas.appendChild(entityCanvas.getCanvas());
-	nodeSectionEditCanvas.appendChild(entityCanvas.filter('white-black'));
+	nodeSectionEditCanvas.appendChild(grayCanvas);
 };
+
+
+function checkGrayFilter(checkCanvas, baseCanvas){
+	var ctx = baseCanvas.getContext('2d'),
+		arrImgColor = ctx.getImageData(0,0, baseCanvas.width, baseCanvas.height),
+		ctxGrayImg  = checkCanvas.getContext('2d'),
+		arrGrayColor = ctxGrayImg.getImageData(0,0, checkCanvas.width, checkCanvas.height);
+
+	for(var i = 0; i < pixelCount*4; i +=4){
+		red = arrImgColor.data[i];
+		green = arrImgColor.data[i + 1];
+		blue = arrImgColor.data[i + 2];
+		gray = Math.floor((red + green + blue)/3);
+
+		if( arrGrayColor.data[i] != gray &&
+			arrGrayColor.data[i + 1] != gray &&
+			arrGrayColor.data[i + 2] != gray
+		){
+			return false;
+		}
+	}
+
+	return true;
+
+}
 
 describe("inCanvas init", function(){
 
@@ -27,4 +54,12 @@ describe("inCanvas init", function(){
 		assert.equal(entityCanvas.getCanvas(), canvas);
 	});
 
+	it('Черно белый фильтр, преобразует цветное изображение в черно-белое', function(){
+		assert.equal(checkGrayFilter(grayCanvas, canvas), true);
+	});
+
+	it('Черно белый фильтр, преобразует цветное изображение в черно-белое (проверяймый канвас не чернобелый)', function(){
+		var defaultCanvas = document.createElement('canvas');
+		assert.equal(checkGrayFilter(defaultCanvas, canvas), false);
+	});
 });
